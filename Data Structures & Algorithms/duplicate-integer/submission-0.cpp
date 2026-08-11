3bb59fc4f9c2f4c844eb08ddd9e3f4acf1340987
yzy123 <31748059+yzy123@users.noreply.github.com>
2026-08-10 22:57:14 -0400

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for (int num : nums) {
+            if (set.count(num)) return true;
+            set.insert(num);
+        }
+        return false;
+    }
+};
---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.count(target - nums[i])) {
+                return {map[target - nums[i]], i};
+            }
+            map[nums[i]] = i;
+        }
+    }
+};
